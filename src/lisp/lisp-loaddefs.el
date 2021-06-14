766ea70a02c15e8511262bde90454a3e7bebecc6
ercdude <eirc.m@protonmail.com>
2021-06-14 17:15:15 -0300

    Add elfeed; Fix some keybinds and minor configs; Add mpv

--- src/lisp/lisp-loaddefs.el
+++ src/lisp/lisp-loaddefs.el
@@ -1483,6 +1483,127 @@ be used:
 
 ;;;***
 
+;;;### (autoloads nil "elfeed" "elfeed/elfeed.el" (0 0 0 0))
+;;; Generated autoloads from elfeed/elfeed.el
+
+(autoload 'elfeed-update "elfeed" "\
+Update all the feeds in `elfeed-feeds'." t nil)
+
+(autoload 'elfeed "elfeed" "\
+Enter elfeed." t nil)
+
+(autoload 'elfeed-load-opml "elfeed" "\
+Load feeds from an OPML file into `elfeed-feeds'.
+When called interactively, the changes to `elfeed-feeds' are
+saved to your customization file.
+
+\(fn FILE)" t nil)
+
+(autoload 'elfeed-export-opml "elfeed" "\
+Export the current feed listing to OPML-formatted FILE.
+
+\(fn FILE)" t nil)
+
+(register-definition-prefixes "elfeed" '("elfeed-"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-csv" "elfeed/elfeed-csv.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from elfeed/elfeed-csv.el
+
+(register-definition-prefixes "elfeed-csv" '("elfeed-csv-"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-curl" "elfeed/elfeed-curl.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elfeed/elfeed-curl.el
+
+(register-definition-prefixes "elfeed-curl" '("elfeed-curl-"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-db" "elfeed/elfeed-db.el" (0 0 0 0))
+;;; Generated autoloads from elfeed/elfeed-db.el
+
+(register-definition-prefixes "elfeed-db" '("elfeed-" "with-elfeed-db-visit"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-lib" "elfeed/elfeed-lib.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from elfeed/elfeed-lib.el
+
+(register-definition-prefixes "elfeed-lib" '("elfeed-"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-link" "elfeed/elfeed-link.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elfeed/elfeed-link.el
+
+(autoload 'elfeed-link-store-link "elfeed-link" "\
+Store a link to an elfeed search or entry buffer.
+
+When storing a link to an entry, automatically extract all the
+entry metadata.  These can be used in the capture templates as
+%:elfeed-entry-<prop>.  See `elfeed-entry--create' for the list
+of available props." nil nil)
+
+(autoload 'elfeed-link-open "elfeed-link" "\
+Jump to an elfeed entry or search.
+
+Depending on what FILTER-OR-ID looks like, we jump to either
+search buffer or show a concrete entry.
+
+\(fn FILTER-OR-ID)" nil nil)
+
+(eval-after-load 'org `(funcall ',(lambda nil (if (version< (org-version) "9.0") (with-no-warnings (org-add-link-type "elfeed" #'elfeed-link-open) (add-hook 'org-store-link-functions #'elfeed-link-store-link)) (with-no-warnings (org-link-set-parameters "elfeed" :follow #'elfeed-link-open :store #'elfeed-link-store-link))))))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-log" "elfeed/elfeed-log.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from elfeed/elfeed-log.el
+
+(register-definition-prefixes "elfeed-log" '("elfeed-log"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-search" "elfeed/elfeed-search.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from elfeed/elfeed-search.el
+
+(autoload 'elfeed-search-bookmark-handler "elfeed-search" "\
+Jump to an elfeed-search bookmarked location.
+
+\(fn RECORD)" nil nil)
+
+(autoload 'elfeed-search-desktop-restore "elfeed-search" "\
+Restore the state of an elfeed-search buffer on desktop restore.
+
+\(fn FILE-NAME BUFFER-NAME SEARCH-FILTER)" nil nil)
+
+(add-to-list 'desktop-buffer-mode-handlers '(elfeed-search-mode . elfeed-search-desktop-restore))
+
+(register-definition-prefixes "elfeed-search" '("elfeed-s"))
+
+;;;***
+
+;;;### (autoloads nil "elfeed-show" "elfeed/elfeed-show.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from elfeed/elfeed-show.el
+
+(autoload 'elfeed-show-bookmark-handler "elfeed-show" "\
+Show the bookmarked entry saved in the `RECORD'.
+
+\(fn RECORD)" nil nil)
+
+(register-definition-prefixes "elfeed-show" '("elfeed-"))
+
+;;;***
+
 ;;;### (autoloads nil "emacsql" "emacsql/emacsql.el" (0 0 0 0))
 ;;; Generated autoloads from emacsql/emacsql.el
 
@@ -7348,6 +7469,13 @@ else like the former.
 
 ;;;***
 
+;;;### (autoloads nil "xml-query" "elfeed/xml-query.el" (0 0 0 0))
+;;; Generated autoloads from elfeed/xml-query.el
+
+(register-definition-prefixes "xml-query" '("xml-query"))
+
+;;;***
+
 ;;;### (autoloads nil "yaml-mode" "yaml-mode/yaml-mode.el" (0 0 0
 ;;;;;;  0))
 ;;; Generated autoloads from yaml-mode/yaml-mode.el
@@ -7436,10 +7564,10 @@ See `yas-minor-mode' for more information on Yas minor mode.
 ;;;### (autoloads nil nil ("all-the-icons/all-the-icons-faces.el"
 ;;;;;;  "cmake-mode-20190710.1319/cmake-mode-autoloads.el" "cmake-mode-20190710.1319/cmake-mode-pkg.el"
 ;;;;;;  "dash.el/dash-functional.el" "dictionary-el/dictionary-init.el"
-;;;;;;  "dictionary-el/lpath.el" "emacs-async/async-pkg.el" "emms/emms-maint.el"
-;;;;;;  "emms/emms-stream-info.el" "forge/forge-pkg.el" "ghub/ghub-pkg.el"
-;;;;;;  "mine/battery.el" "queue/queue-autoloads.el" "queue/queue-pkg.el"
-;;;;;;  "xelb/xelb.el") (0 0 0 0))
+;;;;;;  "dictionary-el/lpath.el" "elfeed/elfeed-pkg.el" "emacs-async/async-pkg.el"
+;;;;;;  "emms/emms-maint.el" "emms/emms-stream-info.el" "forge/forge-pkg.el"
+;;;;;;  "ghub/ghub-pkg.el" "mine/battery.el" "queue/queue-autoloads.el"
+;;;;;;  "queue/queue-pkg.el" "xelb/xelb.el") (0 0 0 0))
 
 ;;;***
 
---
+++ src/lisp/mine/mpv.el
@@ -0,0 +1,44 @@
+;;; mpv.el  --- mpv utilities.
+;;; Commentary:
+;;; Code:
+
+(defvar mpv-video-options '("--osd-level=2")
+  "A list with the options used for displaying video.")
+
+(defun secs-to-mm:ss (n)
+  "Force N - a number of seconds or an \"mm:ss\" string - to the mm:ss format"
+  (if (stringp n) n
+
+    (format "%d:%02d" m s)))
+
+(defun format-mpv-args (fname &optional pos &rest rest)
+  "Forms the argv with the name FNAME and params POS and REST as a list of strings."
+  `("mpv"
+    ,fname
+    ,@(if pos
+          (let* ((s (mod n 60))
+                 (m (/ (- n s) 60)))
+            (list (format "--start=%d:02d" m s))))
+    ,@mpv-video-options
+    ))
+
+(defun play-file (fname &optional pos &rest rest)
+  "Open FNAME with mpv, with a GUI starting at POS seconds and REST as argument."
+  (interactive "fFile name: ")
+  (let ((argv (format-mpv-args fname pos rest)))
+    (apply 'start-process (car argv) "*mpv*" argv)))
+
+(defun play-url (url &optional pos &rest rest)
+  "Open URL with mpv, with a GUI starting at POS seconds and REST as argument."
+  (interactive "sUrl: ")
+  (let ((argv (format-mpv-args url pos rest)))
+    (apply 'start-process (car argv) "*mpv*" argv)))
+
+(defun play-url-at-point (&optional pos &rest rest)
+  "Open url at point with mpv, with a GUI at position POS and REST as argument."
+  (interactive)
+  (let* ((url (thing-at-point 'url t))
+         (argv (format-mpv-args url pos rest)))
+    (apply 'start-process (car argv) "*mpv*" argv)))
+
+;;; mpv.el ends here
